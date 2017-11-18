8f43ad37241553dac1f34f0cdb74cbe2be4ab480
BongGeek <abhinaba@gmail.com>
2017-11-18 00:54:57 +0000

    moved code to the pacakge

--- element.go
+++ element.go
@@ -14,7 +14,7 @@ type Element struct {
 	name     string
 }
 
-func ReadElement(elementStr string) (Element, error) {
+func readElement(elementStr string) (Element, error) {
 	splits := strings.Split(elementStr, ",")
 	atNum, err := strconv.ParseUint(splits[0], 10, 32)
 	if err != nil {
@@ -37,7 +37,7 @@ func LoadPeriodicTable(fileName string, elements map[string]Element){
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
-		element, err := ReadElement(line)
+		element, err := readElement(line)
 		if err != nil {
 			fmt.Println("Failed to parse element", err)
 			panic("Error!!")
@@ -46,3 +46,41 @@ func LoadPeriodicTable(fileName string, elements map[string]Element){
 		elements[strings.ToLower(element.symbol)] = element
 	}
 }
+
+func getElementsForInnerWord(word string, elements map[string]Element, charCount int) []Element{
+	result := make([]Element, 0)
+	s := string(word[0:charCount])
+	if val, ok := elements[s]; ok {
+		newWord := string(word[charCount:])
+		if len(newWord) > 0 {
+			resultTemp := GetElementsForWord(newWord, elements)
+			if len(resultTemp) > 0 {
+				result = append(result, val)
+				result = append(result, resultTemp...)
+			}
+		} else {
+			result = append (result, val)
+		}
+	}
+
+	return result
+}
+
+
+func GetElementsForWord(word string, elements map[string]Element) []Element {
+	result := make([]Element, 0)
+	if len(word) > 0 {
+		//fmt.Println("Word:", word)
+		result = getElementsForInnerWord(word, elements, 1)
+
+		// So using single char as above didn't work lets see
+		// is a pair of chars will work
+		if len(result) == 0 {
+			result = getElementsForInnerWord(word, elements, 2)
+		}
+
+	}
+
+	return result
+}
+
