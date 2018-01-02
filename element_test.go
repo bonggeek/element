14ec25b86f070e1717fd4a197d00025033bb2884
Abhinaba Basu <abhinaba@gmail.com>
2018-01-01 16:42:22 -0800

    Added new elements and tests to verify element list is correct

--- element_test.go
+++ element_test.go
@@ -2,6 +2,7 @@
 package element
 
 import (
+	"strings"
 	"testing"
 	"os"
 	"bufio"
@@ -34,13 +35,14 @@ func TestAllWords(t *testing.T){
 
 		if len(res) > 0 {
 			foundCount++
-			fmt.Print(line, " - ")
+			/*fmt.Print(line, " - ")
 			for _, k := range res {
 				fmt.Print(k.Symbol, " ")
 			}
 			fmt.Println()
+			*/
 		} else {
-			fmt.Println("!===================", line)
+			//fmt.Println("!===================", line)
 			notFoundCount++
 		}
 	}
@@ -67,4 +69,59 @@ func TestAllElements(t *testing.T){
 		t.Error("Count of elements ")
 	}
 
+}
+
+func TestAllElementTags(t *testing.T){
+	elements := GetElements().GetAllElements()
+	validProperties := map[string]bool{
+		"adaptable": true,
+		"attractive": true,
+		"available": true,
+		"bold": true,
+		"brilliant": true,
+		"creative": true,
+		"cooperative": true,
+		"colorful": true,
+		"complex": true,
+		"dependable": true,
+		"earthy": true,
+		"explosive": true,
+		"fickle": true,
+		"fair": true,
+		"generous": true,
+		"honest": true,
+		"lovable": true,
+		"modest": true,
+		"noble": true,
+		"rich": true,
+		"solid": true,
+		"simple": true,
+		"toxic": true,
+	}
+	validPropWeight := map[string]bool{
+		"lo": true,
+		"hi": true,
+	}
+
+	for _,e := range elements{
+		// TODO: Some elements properties are still not set
+		if len(e.Properties) > 0 {
+			props := strings.Split(e.Properties, ",")
+			for _,prop := range props{
+				ind := strings.Split(prop, "-")
+				if len(ind) < 2{
+					t.Errorf("Failed on %v(%v) for %v", e.Name, e.AtNumber, ind)
+				} else {
+					if !validProperties[ind[0]] {
+						t.Errorf("%v(%v) => %v is not a valid property", e.Name, prop, ind[0])
+					}
+					
+					if !(validPropWeight[ind[1]]){
+						t.Errorf("%v(%v) => %v is not a valid property weight", e.Name, prop,  ind[1])
+					}
+				}
+			}
+		}
+		//fmt.Println(e.Properties)
+	}
 }
